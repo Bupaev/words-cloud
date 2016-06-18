4f5be19c281ee7066769eb456410caaeda5f3a03
Pavel Buramensky <bupaev@gmail.com>
2016-06-19 05:11:35 +0800

    Calculate word size depends on area and words amount

--- app/scripts/d3cloud.js
+++ app/scripts/d3cloud.js
@@ -1,51 +1,73 @@
 'use strict';
 
+/**
+ * 1. Add popups with information to specific words
+ * 2. Add hierarchy of cloud levels
+ * 3. Add navigation in cloud
+ * 4. Keep generated SVG to navigation between cloud levels w/o SVG regenerating
+ */
+
 console.log('3d cloud.js is loaded');
 
-var wordsArray = [
-  { text: 'Front-end', size: 50 },
-  { text: 'JavaScript', size: 50 },
-  { text: 'AngularJS', size: 30 },
-  { text: 'jQuery', size: 20 },
-  { text: 'Vanila JS', size: 40 },
-  { text: 'eslint', size: 20 },
-  { text: 'LESS', size: 30 },
-  { text: 'SCSS', size: 20 },
-  { text: 'Web Audio API', size: 40 },
-  { text: 'Canvas', size: 20 },
-  { text: 'SVG', size: 20 },
-  { text: 'Gulp', size: 30 },
-  { text: 'npm', size: 30 },
-  { text: 'NodeJS', size: 20 },
-  { text: 'Unit testing', size: 20 },
-  { text: 'TypeScript', size: 20 },
-  { text: 'WebStorm', size: 50 },
-  { text: 'Atom', size: 30 },
-  { text: 'GIT', size: 40 },
-  { text: 'SVN', size: 20 },
-  { text: 'TFS', size: 20 },
-  { text: 'HTML5', size: 50 },
-  { text: 'ES6', size: 40 },
-  { text: 'Lodash', size: 40 },
-  { text: 'Photoshop', size: 30 },
-  { text: 'Lightroom', size: 30 },
-  { text: 'Photography', size: 30 },
-  { text: 'UI UX', size: 30 },
-  { text: 'Datamining', size: 20 },
-  { text: 'SQL', size: 20 },
-  { text: 'Matlab', size: 20 },
-  { text: '3Ds Max', size: 20 },
-  { text: 'Video editing', size: 20 },
-  { text: 'Agile', size: 20 },
-  { text: 'English', size: 20 },
-  { text: 'English', size: 30 },
-  { text: 'Omsk', size: 30 },
-  { text: 'Singapore', size: 20 }];
+let wordsArray = [
+  { text: 'Front-end', size: 5 },
+  { text: 'JavaScript', size: 5 },
+  { text: 'AngularJS', size: 3 },
+  { text: 'jQuery', size: 2 },
+  { text: 'Vanila JS', size: 4 },
+  { text: 'eslint', size: 2 },
+  { text: 'LESS', size: 3 },
+  { text: 'SCSS', size: 2 },
+  { text: 'Web Audio API', size: 4 },
+  { text: 'Canvas', size: 2 },
+  { text: 'SVG', size: 2 },
+  { text: 'Gulp', size: 3 },
+  { text: 'npm', size: 3 },
+  { text: 'NodeJS', size: 2 },
+  { text: 'Unit testing', size: 2 },
+  { text: 'TypeScript', size: 2 },
+  { text: 'WebStorm', size: 5 },
+  { text: 'Atom', size: 3 },
+  { text: 'GIT', size: 4 },
+  { text: 'SVN', size: 2 },
+  { text: 'TFS', size: 2 },
+  { text: 'HTML5', size: 5 },
+  { text: 'ES6', size: 4 },
+  { text: 'Lodash', size: 4 },
+  { text: 'Photoshop', size: 3 },
+  { text: 'Lightroom', size: 3 },
+  { text: 'Photography', size: 3 },
+  { text: 'UI UX', size: 3 },
+  { text: 'Datamining', size: 2 },
+  { text: 'SQL', size: 2 },
+  { text: 'Matlab', size: 2 },
+  { text: '3Ds Max', size: 2 },
+  { text: 'Video editing', size: 2 },
+  { text: 'Agile', size: 2 },
+  { text: 'English', size: 2 },
+  { text: 'English', size: 3 },
+  { text: 'Omsk', size: 3 },
+  { text: 'Singapore', size: 2 },
+  { text: 'Bower', size: 3 },
+  { text: 'Browserify', size: 2 }];
 
-var fill = d3.scale.category20c();
+const svgWidth = 800;
+const svgHeight = 600;
 
-var layout = d3.layout.cloud()
-  .size([800, 500])
+function calcWordsSize(array) {
+  const svgArea = svgWidth * svgHeight;
+  return array.map(function(word) {
+    word.size *= svgArea / wordsArray.length / 6 *  Math.sqrt(wordsArray.length) / Math.pow(svgArea, 1 / 1.9);
+    return word;
+  })
+}
+
+wordsArray = calcWordsSize(wordsArray);
+
+const fill = d3.scale.category20c();
+
+const layout = d3.layout.cloud()
+  .size([svgWidth, svgHeight])
   .words(wordsArray)
   .padding(5)
   .rotate(function() {
@@ -81,8 +103,11 @@ function draw(words) {
     })
     .text(function(d) {
       return d.text;
+    })
+    .on('mouseover', function(data) {
+      console.log('Press', data.text);
     });
 }
 
-var textsElements = document.getElementsByTagName('text');
+const textsElements = document.getElementsByTagName('text');
 console.log('Data length:', wordsArray.length, 'Cloud size:', textsElements.length);
