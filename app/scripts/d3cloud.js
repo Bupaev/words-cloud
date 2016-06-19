f6c93d96edcbb515d0673d204c190b32fccff348
Pavel Buramensky <bupaev@gmail.com>
2016-06-20 01:02:24 +0800

    Add d3 tip

--- app/scripts/d3cloud.js
+++ app/scripts/d3cloud.js
@@ -1,17 +1,18 @@
 'use strict';
 
 /**
- * 1. Add popups with information to specific words
- * 2. Add hierarchy of cloud levels
- * 3. Add navigation in cloud
- * 4. Keep generated SVG to navigation between cloud levels w/o SVG regenerating
+ * 1. Add smart words scaling (+)
+ * 2. Add popups with information to specific words
+ * 3. Add hierarchy of cloud levels
+ * 4. Add navigation in cloud
+ * 5. Keep generated SVG to navigation between cloud levels w/o SVG regenerating
  */
 
 console.log('3d cloud.js is loaded');
 
 let wordsArray = [
-  { text: 'Front-end', size: 5 },
-  { text: 'JavaScript', size: 5 },
+  { text: 'Front-end', size: 5, desc: 'Front-end is my life', img: 'images/chrome-logo.jpg' },
+  { text: 'JavaScript', size: 5, desc: 'I like Javascript and everything related to this language', img: 'images/js-logo.png' },
   { text: 'AngularJS', size: 3 },
   { text: 'jQuery', size: 2 },
   { text: 'Vanila JS', size: 4 },
@@ -53,6 +54,7 @@ let wordsArray = [
 
 const svgWidth = 800;
 const svgHeight = 600;
+let svgContext = {};
 
 function calcWordsSize(array) {
   const svgArea = svgWidth * svgHeight;
@@ -65,6 +67,14 @@ function calcWordsSize(array) {
 wordsArray = calcWordsSize(wordsArray);
 
 const fill = d3.scale.category20c();
+
+/* Initialize tooltip */
+const tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset([-10, 0])
+  .html(function(d) {
+    return "<img src=" + (d.img || 'images/js-logo.png') + "><span>" + (d.desc || 'default') + "</span>";
+  });
 
 const layout = d3.layout.cloud()
   .size([svgWidth, svgHeight])
@@ -82,7 +92,7 @@ const layout = d3.layout.cloud()
 layout.start();
 
 function draw(words) {
-  d3.select('body').append('svg')
+  svgContext = d3.select('body').append('svg')
     .attr('width', layout.size()[0])
     .attr('height', layout.size()[1])
     .append('g')
@@ -101,13 +111,27 @@ function draw(words) {
     .attr('transform', function(d) {
       return 'translate(' + [d.x, d.y] + ')rotate(' + d.rotate + ')';
     })
+    .attr('description-text', function(d) {
+      return d.desc;
+    })
+    .attr('description-image', function(d) {
+      return d.img;
+    })
     .text(function(d) {
       return d.text;
     })
-    .on('mouseover', function(data) {
-      console.log('Press', data.text);
+    .on('mouseover', function(d) {
+      tip.attr('class', 'd3-tip animate').show(d)
+    })
+    .on('mouseout', function(d) {
+      tip.attr('class', 'd3-tip').show(d);
+      tip.hide();
     });
 }
+
+/* Invoke the tip in the context of your visualization */
+svgContext.call(tip);
+// d3.select(".d3-tip").remove();
 
 const textsElements = document.getElementsByTagName('text');
 console.log('Data length:', wordsArray.length, 'Cloud size:', textsElements.length);
